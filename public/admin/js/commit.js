e1b77f8e84b619bdd366e220b01d255f50fb852f
zhangxiong922 <3465417484@qq.com>
2019-03-21 14:32:21 +0800

    index完成

--- public/admin/js/commit.js
+++ public/admin/js/commit.js
@@ -1,6 +1,48 @@
-$(document).ajaxStart(function() {
+
     NProgress.start();
-})
-$(document).ajaxStop(function() {
-    NProgress.done();
-})
+    setTimeout(function() {
+        NProgress.done();
+    },500)
+    $(document).ajaxStart(function() {
+        NProgress.start();
+    })
+    $(document).ajaxStop(function() {
+        setTimeout(function() {
+            NProgress.done();
+        },500)
+    })
+    $(".nn").on('click',function() {
+
+        $('.non').stop().slideToggle()
+      // console.log(1)
+      })
+      var as = $("aside a");
+      
+  
+  
+  
+  
+      $(".top .zuo").on('click',function() {
+        $('.aside').toggleClass('xiao');
+        $('.top').toggleClass('ye');
+        $('.main').toggleClass('ye');
+      })
+  
+  
+      $('.top .you .out').on('click',function() {
+        $('#myModel').modal('show')
+      })
+  
+  
+      $('.tui').on('click',function() {
+        $.ajax({
+          type:'get',
+          url:'/employee/employeeLogout',
+          success:function(info){
+            // console.log(info)
+              if(info.success) {
+                location.href = 'login.html';
+              }
+          }
+        })
+      })
